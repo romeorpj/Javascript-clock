5ee46fe66fa62ac933edfd3fe2fcca7c88a9f451
richardj-codes <74265663+richardj-codes@users.noreply.github.com>
2021-12-20 12:11:20 -0600

    updated hour hand logic and readme file

--- app2.js
+++ app2.js
@@ -1,39 +1,31 @@
-// select hands from dom
+// Add media query
 
+// select hands from dom
 let sHand = document.querySelector(".sh");
 let mHand = document.querySelector(".mh");
 let hHand = document.querySelector(".hh");
 let period = document.querySelector(".period");
 let time2 = new Date();
-// Come back and add an am/pm indicator***
 
 // Create clock function and time object
 let second2, minute2, hour2;
+
 let clock2Function = () => {
 	// min and seconds times 6 = 6*60 = 360deg
-	// hour is 12 times 30 = 360deg
+
 	let time2 = new Date();
-	second2 = time2.getSeconds() * 6;
-	minute2 = time2.getMinutes() * 6;
+	second2 = time2.getSeconds();
+	minute2 = time2.getMinutes();
 	hour2 = time2.getHours();
-	hour2Deg = hour2 * 30 + (second2 * 300) / 43200;
+
+	hour2Deg = hour2 * 30 + minute2 * 0.5;
 	hour2 > 12 ? (period.textContent = "pm") : (period.textContent = "am");
 
 	// rotate hands
-	sHand.style.transform = `rotate(${second2}deg)`;
-	mHand.style.transform = `rotate(${minute2}deg)`;
+	sHand.style.transform = `rotate(${second2 * 6}deg)`;
+	mHand.style.transform = `rotate(${minute2 * 6}deg)`;
 	hHand.style.transform = `rotate(${hour2Deg}deg)`;
 };
 
-// rotate function
-// original idea was going to update custom property
-// but i found an easier way
-
-// let rotateFunc = (element, degrees) => {
-// 	// element.setProperty("--hh-rotate", "30deg");
-// 	element.style.setProperty("--rotate", degrees * 360);
-// };
-
-// domcontentloaded so the clock starts on the right time as soon as the page loads
 document.addEventListener("DOMContentLoaded", clock2Function);
 setInterval(clock2Function, 1000);
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 two clocks, one analog with roatating hands.
 one digital with changing numbers.
+
+- I could have definitely used one js file, but I just wanted to keep things separate for ease and less confusion.
+
+- i first started this for practice, didnt plan on anything being responsive, but the perfectionist in me wont let go without making it responsive, So i'll come back and add media queries and we're done.
+
+- calculate clock hand degrees
+  -- minute and second hands were simple. just multiply them by 6 because 60 seconds in an minute(60 mins in an hour), and we need the hands to rotate 360degrees. so 360/60 = 6. so just take the current seconds and add 6 degrees to place it correctly on the clock. same for the minute hand.
+  -- the hour hand was a bit more tricky. Since there are 60 minutes in an hour, and the hour hand moves 30 degrees every 60 minutes, that means that the hour hand moves 30 / 60, or 0.5 degrees every minute. Therefore, the angle of the hour hand can be calculated by multiplying the hour by 30, multiplying the minutes by 0.5, and adding those two results. If we were given the time 8:15, we'd know the hour hand is a bit past the 8. When it's at the 8, the hour hand is 8 _ 30, or 240 degrees past the 12. The fifteen minutes are another 15 _ 0.5, or 7.5 degrees, more. In total, therefore, at 8:15, the hour hand is at an angle of 247.5 degrees.
+
+-domContentLoaded so the clock starts on the right time as soon as the page loads
